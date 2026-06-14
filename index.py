f8709035bd24d9d1ca0074e68dc056e52ab6ab16
Jeff Xie <37353357+lumosityfan@users.noreply.github.com>
2026-06-14 16:11:12 -0400

    Add input form button and processing of form data in flask backend

--- index.py
+++ index.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, jsonify
+from flask import Flask, request, render_template, jsonify
 from flask_limiter import Limiter
 from flask_limiter.util import get_remote_address
 import requests
@@ -20,28 +20,38 @@ load_dotenv()
 api_key = os.environ.get('API_KEY')
 
 @app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/weather')
 @limiter.limit("10 per minute")  # Limit this route to 10 requests per minute
-def index():
-    url = f"https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/London,UK?key={api_key}"
+def weather():
+    latitude = request.args.get('latitude', '0')  # Default to '0' if not provided
+    print(latitude)
+    longitude = request.args.get('longitude', '0')  # Default to '0' if not provided
+    print(longitude)
+    url = f"https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/{latitude},{longitude}?key={api_key}"
     try:
         response = requests.get(url)
         if response.status_code == 200:
             data = response.json()
+            resolvedAddress = data.get("resolvedAddress", "N/A") + " (latitude, longitude)"
             # Extract specific data points
+            print(resolvedAddress)
             weather_data = {
-                "resolvedAddress": data["resolvedAddress"],
+                "resolvedAddress": resolvedAddress,
                 "description": data["description"],
                 "currentConditions": data["currentConditions"],
                 "days": data["days"][:7]  # Get the first 7 days of forecast
             }
             # Pass the data dict to your HTML template
-            return render_template('index.html', weather=weather_data)
+            return render_template('weather.html', weather=weather_data)
         elif response.status_code == 429:
-            return render_template('index.html', error="Rate limit exceeded. Please try again later.")
+            return render_template('weather.html', error="Rate limit exceeded. Please try again later.")
         else:
-            return render_template('index.html', error="Could not retrieve weather data.")
+            return render_template('weather.html', error="Could not retrieve weather data.")
     except Exception as e:
-        return render_template('index.html', error="Connection error occurred.")
+        return render_template('weather.html', error="Connection error occurred.")
 
 if __name__ == '__main__':
     app.run(debug=True)  
